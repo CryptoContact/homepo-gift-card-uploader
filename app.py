5c478427e70598b266a8fcff41cfed129772b5bd
Your Name <you@example.com>
2020-04-17 08:36:16 +0200

    moved from zappa/aws to local hosting / celery

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+
+from celery import Celery
+
+celery_app = Celery('gift_card_uploader', broker='pyamqp://guest@localhost//')
+
+
 from flask import request
 from init import app, db
-from zappa.asynchronous import task
 from homepoTools import (
 	is_valid_homedepot_gift_card,
 	commit, 
@@ -8,7 +13,7 @@ from homepoTools import (
 	convert_from_request_to_gift_card
 )
 
-@task
+@celery_app.task
 def add_cards_to_database(chunks_of_cards):
 	for card in chunks_of_cards:
 		card = convert_from_request_to_gift_card(card)
@@ -20,11 +25,9 @@ def add():
 	cards = request.get_json()
 	chunks_of_cards = [cards[x:x+500] for x in range(0, len(cards), 100)]
 	for smaller_chunk_of_cards in chunks_of_cards:
-		add_cards_to_database(smaller_chunk_of_cards)	
-	
+		add_cards_to_database(smaller_chunk_of_cards)
+
 	return {"success": True}
 
-
-
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(host="0.0.0.0", debug=True)
